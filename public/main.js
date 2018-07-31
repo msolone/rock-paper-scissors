7dbcd685657eef039b46e9495b947962a22e4c56
Michael Solone <michaelsolone@gmail.com>
2018-07-31 14:19:16 -0400

    all 3 buttons functioning in console

--- public/main.js
+++ public/main.js
@@ -5,23 +5,17 @@
 // document.addEventListener('DOMContentLoaded', main)
 
 
-// const rock_paper_scissors_dict = {
-//   1: 'rock'
-//   2: 'paper'
-//   3: 'scissors'
-// }
+
 
 const choices = ['rock','paper','scissors']
 
-const CPU_choice = choices[Math.floor(Math.random() * choices.length)]
-
-
-
 const p1_pick_rock = () => {
+    // Set CPU choice
+    const CPU_choice = choices[Math.floor(Math.random() * choices.length)]
     // Set p1 choice
     const p1_choice = "rock"
     console.log('Player 1s choice is ' + p1_choice)
-    console.log('Player 1s choice is ' + CPU_choice)
+    console.log('The CPU choice is ' + CPU_choice)
     // if else statement comparing p1 choice vs CPU choice
     if (CPU_choice === 'rock') {
     console.log('You tied, try again!')
@@ -37,20 +31,60 @@ const p1_pick_rock = () => {
 document.querySelector('.rock').addEventListener('click', p1_pick_rock)
 
 
+const p1_pick_scissors = () => {
+  // Set CPU choice
+  const CPU_choice = choices[Math.floor(Math.random() * choices.length)]
+  // Set p1 choice
+  const p1_choice = "scissors"
+  console.log('Player 1s choice is ' + p1_choice)
+  console.log('The CPU choice is ' + CPU_choice)
+  // if else statement comparing p1 choice vs CPU choice
+  if (CPU_choice === 'scissors') {
+  console.log('You tied, try again!')
+    } else {
+      if (CPU_choice === 'paper') {
+        console.log('You win!')
+      } else {
+        console.log('You lose!')
+      }
+    }
+  }
+
+document.querySelector('.scissors').addEventListener('click', p1_pick_scissors)
 
 
 
 
 const p1_pick_paper = () => {
+  // Set CPU choice
+  const CPU_choice = choices[Math.floor(Math.random() * choices.length)]
+  // Set p1 choice
   const p1_choice = "paper"
   console.log('Player 1s choice is ' + p1_choice)
-}  
+  console.log('The CPU choice is ' + CPU_choice)
+  // if else statement comparing p1 choice vs CPU choice
+  if (CPU_choice === 'paper') {
+  console.log('You tied, try again!')
+    } else {
+      if (CPU_choice === 'rock') {
+        console.log('You win!')
+      } else {
+        console.log('You lose!')
+      }
+    }
+  }
 
 document.querySelector('.paper').addEventListener('click', p1_pick_paper)
 
-const p1_pick_scissors = () => {
-  const p1_choice = "scissors"
-  console.log('Player 1s choice is ' + p1_choice)
-}  
 
-document.querySelector('.scissors').addEventListener('click', p1_pick_scissors)
+
+
+
+
+
+
+
+
+
+
+
