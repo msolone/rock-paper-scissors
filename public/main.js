c65c271a56824128c02ee7669bcbec1825ef267c
Michael Solone <michaelsolone@gmail.com>
2018-07-31 13:46:17 -0400

    p1 choices logged

--- public/main.js
+++ public/main.js
@@ -1,5 +1,30 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+// const main = () => {
+//   document.querySelector('h1').textContent += '?'
+// }
 
-document.addEventListener('DOMContentLoaded', main)
+// document.addEventListener('DOMContentLoaded', main)
+
+
+
+
+
+const p1_pick_rock = () => {
+    const p1_choice = "Rock"
+    console.log('Player 1s choice is ' + p1_choice)
+}  
+
+document.querySelector('.rock').addEventListener('click', p1_pick_rock)
+
+const p1_pick_paper = () => {
+  const p1_choice = "Paper"
+  console.log('Player 1s choice is ' + p1_choice)
+}  
+
+document.querySelector('.paper').addEventListener('click', p1_pick_paper)
+
+const p1_pick_scissors = () => {
+  const p1_choice = "scissors"
+  console.log('Player 1s choice is ' + p1_choice)
+}  
+
+document.querySelector('.scissors').addEventListener('click', p1_pick_scissors)
