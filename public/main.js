c9da314554f2b5c79623fc906cc5217b3b978091
Michael Solone <michaelsolone@gmail.com>
2018-07-31 14:10:32 -0400

    working for rock pick

--- public/main.js
+++ public/main.js
@@ -5,18 +5,44 @@
 // document.addEventListener('DOMContentLoaded', main)
 
 
+// const rock_paper_scissors_dict = {
+//   1: 'rock'
+//   2: 'paper'
+//   3: 'scissors'
+// }
+
+const choices = ['rock','paper','scissors']
+
+const CPU_choice = choices[Math.floor(Math.random() * choices.length)]
 
 
 
 const p1_pick_rock = () => {
-    const p1_choice = "Rock"
+    // Set p1 choice
+    const p1_choice = "rock"
     console.log('Player 1s choice is ' + p1_choice)
-}  
+    console.log('Player 1s choice is ' + CPU_choice)
+    // if else statement comparing p1 choice vs CPU choice
+    if (CPU_choice === 'rock') {
+    console.log('You tied, try again!')
+      } else {
+        if (CPU_choice === 'scissors') {
+          console.log('You win!')
+        } else {
+          console.log('You lose!')
+        }
+      }
+    }
 
 document.querySelector('.rock').addEventListener('click', p1_pick_rock)
 
+
+
+
+
+
 const p1_pick_paper = () => {
-  const p1_choice = "Paper"
+  const p1_choice = "paper"
   console.log('Player 1s choice is ' + p1_choice)
 }  
 
